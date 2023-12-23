2a4af66e6e4bef2329242d47d2772d4d54df8a05
Krabbens <s188660@student.pg.edu.pl>
2023-12-23 19:20:16 +0100

    Refactor Palette class initialization

--- src/palette.py
+++ src/palette.py
@@ -5,19 +5,7 @@ from qqt import qqtDictModel
 class Palette(qqtDictModel):
     '''Palette is a model for color palette.'''
     def __init__(self, model_name, parent=None) -> None:
-        headers = [
-            "bg", "fg", "dv", "fo",
-            "con1", "con2", "con3", "con4",
-            "def", "pri", "sec", "suc", "war", "dan",
-            "def-50", "def-100", "def-200", "def-300", "def-400", "def-500", "def-600", "def-700", "def-800", "def-900",
-            "pri-50", "pri-100", "pri-200", "pri-300", "pri-400", "pri-500", "pri-600", "pri-700", "pri-800", "pri-900",
-            "sec-50", "sec-100", "sec-200", "sec-300", "sec-400", "sec-500", "sec-600", "sec-700", "sec-800", "sec-900",
-            "suc-50", "suc-100", "suc-200", "suc-300", "suc-400", "suc-500", "suc-600", "suc-700", "suc-800", "suc-900",
-            "war-50", "war-100", "war-200", "war-300", "war-400", "war-500", "war-600", "war-700", "war-800", "war-900",
-            "dan-50", "dan-100", "dan-200", "dan-300", "dan-400", "dan-500", "dan-600", "dan-700", "dan-800", "dan-900",
-        ]
-        super().__init__("ui", headers, parent)
-        self.set_dict({
+        palette = {
             "bg": "#000000", "fg": "#ECEDEE", "div": "26FFFFFF",
             "con1": "#18181B", "con2": "#27272A", "con3": "#3F3F46", "con4": "#52525B",
             "def": "#3F3F46", "pri": "#006FEE", "sec": "#9353D3", "suc": "#17C964", "war": "#F5A524", "dan": "#F31260",
@@ -27,4 +15,6 @@ class Palette(qqtDictModel):
             "suc-50": "#052814", "suc-100": "#095028", "suc-200": "#0E793C", "suc-300": "#12A150", "suc-400": "#17C964", "suc-500": "#45D483", "suc-600": "#74DFA2", "suc-700": "#A2E9C1", "suc-800": "#D1F4E0", "suc-900": "#E8FAF0",
             "war-50": "#312107", "war-100": "#62420E", "war-200": "#936316", "war-300": "#C4841D", "war-400": "#F5A524", "war-500": "#F7B750", "war-600": "#F9C97C", "war-700": "#FBDBA7", "war-800": "#FDEDD3", "war-900": "#FEFCE8",
             "dan-50": "#310413", "dan-100": "#610726", "dan-200": "#920B3A", "dan-300": "#C20E4D", "dan-400": "#F31260", "dan-500": "#F54180", "dan-600": "#F871A0", "dan-700": "#FAA0BF", "dan-800": "#FDD0DF", "dan-900": "#FEE7EF",
-        })
+        }
+        super().__init__("ui", list(palette.keys()), parent)
+        self.set_dict(palette)
